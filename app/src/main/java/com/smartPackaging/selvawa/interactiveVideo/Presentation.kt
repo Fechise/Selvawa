65eb112c0d3bb87cb8f4aa7bac712f9c5375baae
Andrés Mateo Quilupangui Osorio <andres.quillupangui@epn.edu.ec>
2025-06-13 08:22:21 -0500

    code(master): Create package interactiveVideo.

---
+++ app/src/main/java/com/smartPackaging/selvawa/interactiveVideo/Presentation.kt
@@ -0,0 +1,21 @@
+package com.smartPackaging.selvawa.interactiveVideo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.smartPackaging.selvawa.game.R
+
+class Presentation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_presentation)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
