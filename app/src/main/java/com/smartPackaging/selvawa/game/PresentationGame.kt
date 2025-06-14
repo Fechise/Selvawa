a6927a50eb0f193b11325c49186e5f5eba174a99
Andrés Mateo Quilupangui Osorio <andres.quillupangui@epn.edu.ec>
2025-06-14 00:31:19 -0500

    code(master): Create more activities for the games, and finish the presentation screen.

--- app/src/main/java/com/smartPackaging/selvawa/game/PresentationGame.kt
+++ app/src/main/java/com/smartPackaging/selvawa/game/PresentationGame.kt
@@ -1,20 +1,42 @@
 package com.smartPackaging.selvawa.game
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class PresentationGame : AppCompatActivity() {
+
+    private lateinit var buttonAJugar : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_presentation_game)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIComponents()
+        initEventListeners()
+    }
+
+    private fun initUIComponents() {
+        buttonAJugar = findViewById(R.id.buttonAJugar)
+    }
+
+    private fun initEventListeners() {
+        buttonAJugar.setOnClickListener {
+            startActivity(Intent(this, MultipleChoiceQuestion::class.java))
+            finish()
         }
     }
 }
--- app/src/main/java/com/smartPackaging/selvawa/game/TrueOrFalseQuestion.kt
+++ app/src/main/java/com/smartPackaging/selvawa/game/TrueOrFalseQuestion.kt
@@ -1,17 +1,16 @@
-package com.smartPackaging.selvawa.interactiveVideo
+package com.smartPackaging.selvawa.game
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.smartPackaging.selvawa.game.R
 
-class Presentation : AppCompatActivity() {
+class TrueOrFalseQuestion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_presentation)
+        setContentView(R.layout.activity_true_or_false_question)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
